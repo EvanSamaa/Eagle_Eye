962138af32cde6e3db95954374ca0c92db7db8fc
Abdulnaser97 <42423897+Abdulnaser97@users.noreply.github.com>
2018-08-19 13:54:29 -0400

    Here is the initial Code, Naser

---
+++ EagleEyeCode.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bool.h>
+//#define upwardspike, downwardsoike, ports, ....
+int main(int argc, char **argv)
+{
+while(true){
+    
+    if(Port#1 > 600 ){
+        bool.upwardSpike = 1;
+        row+20;
+        if(port#1 < 200 && upwardspike =1){
+            Printf("looking down");
+            //repeat while loop}
+    }
+    
+    if(Port#1 < 200 ){
+            bool.upwardSpike = 1;
+            row+20;
+            if(port#1 > 600 && upwardspike =1){
+                Printf("looking up");
+                //repeat while loop;
+    }
+    
+    
+    if(Port#2 < 200 ){
+        bool.upwardSpike = 1;
+        row+20;
+        if(port#2 > 600 && upwardspike =1){
+          Printf("looking Left");
+          //repeat while loop;
+        }
+        
+    }
+    if(Port#2 > 600 ){
+        bool.upwardSpike = 1;
+        row+20;
+        if(port#2 < 200 && upwardspike =1){
+        Printf("looking Right");
+        //repeat while loop;
+        }
+    }
+	return 0;
+    
+    
+    if(Port#0 < 600 && Port#3 < 600){
+        bool.downwardspike = 1;
+        row+20;
+        if(port#0 > 100 && port#3 > 100 && downwardspike =1){
+        Printf("Blink");
+        //repeat while loop;
+        }
+    }
+    
+}
+    }
