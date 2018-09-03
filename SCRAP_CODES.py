0cdf36ee44cac1b058afa9c7e60ca7416ae15bb5
Shi Jie Wei <barneywei@hotmail.com>
2018-09-03 08:48:21 -0400

    Created: plot_and_analyze_csv.py

--- SCRAP_CODES.py
+++ SCRAP_CODES.py
@@ -1,2 +1,11 @@
-list = [[1,2,3,4,6], [1,2,3,4,5]]
-print(list[1,:])
+import numpy as np
+
+
+list = np.zeros((0,))
+list1 = [1,2,3,4]
+list2 = [9,6,7,8]
+
+hi = np.append(list, [list1], axis=0)
+hi2 = np.append(hi, [list2], axis=0)
+print(hi)
+print(hi2)
